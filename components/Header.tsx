932d78199e1a7606a7e9078b51a0905262904087
Paul Buscano <pbuscano@deloitte.com>
2022-10-10 15:05:41 +0800

    Fix when resize window and outside dropdown mobile menu hide it

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,79 +1,157 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function Header() {
+  const [active, setActive] = useState(false);
+  const dropdown = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdown.current && !dropdown.current.contains(event.target)) {
+        setActive(false);
+      }
+    }
+    function handleResize() {
+      setActive(false);
+    }
+    // Bind the event listener
+    window.addEventListener("resize", handleResize);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [dropdown]);
+
   return (
-    <header className="relative bg-secondary-color">
-      <div className="max-w-screen-2xl mx-auto flex flex-row items-center justify-between px-4 py-3 w-full">
-        {/* logo */}
-        <Link href="/">
-          <div className="cursor-pointer transition hover:opacity-80 w-40">
+    <header>
+      <div className="relative bg-secondary-color">
+        <div className="max-w-screen-2xl mx-auto flex flex-row items-center justify-between px-2 py-3 w-full">
+          {/* logo */}
+          <Link href="/">
             <Image
+              className="cursor-pointer hover:opacity-80"
               src={require("../styles/images/logo.png")}
-              objectFit="contain"
+              width={200}
+              height={70}
             />
+          </Link>
+          {/* mobile menu */}
+          <div className="md:hidden">
+            <button onClick={() => setActive(!active)}>
+              <Image
+                className="cursor-pointer hover:opacity-80"
+                src={require("../styles/images/logo_menu.png")}
+                width={30}
+                height={30}
+              />
+            </button>
           </div>
-        </Link>
-        {/* menu */}
-        <div className="flex flex-row items-center justify-center invisible lg:visible">
-          <div className="px-4">
+          {/* menu */}
+          <div className="hidden md:flex">
+            <div className="flex flex-row items-center justify-center">
+              <div className="px-4">
+                <Link href="/">
+                  <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
+                    News
+                  </h1>
+                </Link>
+              </div>
+              <div className="px-4">
+                <Link href="/">
+                  <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
+                    Schedule
+                  </h1>
+                </Link>
+              </div>
+              <div className="px-4">
+                <Link href="/">
+                  <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
+                    Teams
+                  </h1>
+                </Link>
+              </div>
+              <div className="px-4">
+                <Link href="/">
+                  <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
+                    Standings
+                  </h1>
+                </Link>
+              </div>
+              <div className="px-4">
+                <Link href="/">
+                  <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
+                    Stats
+                  </h1>
+                </Link>
+              </div>
+              <div className="px-4">
+                <Link href="/">
+                  <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
+                    Videos
+                  </h1>
+                </Link>
+              </div>
+              <div className="px-4">
+                <Link href="/">
+                  <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
+                    About
+                  </h1>
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* search */}
+          <div className="hidden md:flex">
             <Link href="/">
               <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
-                News
-              </h1>
-            </Link>
-          </div>
-          <div className="px-4">
-            <Link href="/">
-              <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
-                Schedule
-              </h1>
-            </Link>
-          </div>
-          <div className="px-4">
-            <Link href="/">
-              <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
-                Teams
-              </h1>
-            </Link>
-          </div>
-          <div className="px-4">
-            <Link href="/">
-              <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
-                Standings
-              </h1>
-            </Link>
-          </div>
-          <div className="px-4">
-            <Link href="/">
-              <h1 className="cursor-pointer text-white">Stats</h1>
-            </Link>
-          </div>
-          <div className="px-4">
-            <Link href="/">
-              <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
-                Videos
-              </h1>
-            </Link>
-          </div>
-          <div className="px-4">
-            <Link href="/">
-              <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
-                About
+                Contact
               </h1>
             </Link>
           </div>
         </div>
-        {/* search */}
-        <div>
+      </div>
+      {active && (
+        <div ref={dropdown} className="bg-yellow-500 px-4 pb-5">
+          <Link href="/news">
+            <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-black py-2 border-b border-black">
+              News
+            </h1>
+          </Link>
           <Link href="/">
-            <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-primary-color">
-              Search
+            <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-black py-2 border-b border-black">
+              Schedule
+            </h1>
+          </Link>
+          <Link href="/">
+            <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-black py-2 border-b border-black">
+              Teams
+            </h1>
+          </Link>
+          <Link href="/">
+            <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-black py-2 border-b border-black">
+              Standings
+            </h1>
+          </Link>
+          <Link href="/">
+            <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-black py-2 border-b border-black">
+              Videos
+            </h1>
+          </Link>
+          <Link href="/">
+            <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-black py-2 border-b border-black">
+              About
+            </h1>
+          </Link>
+          <Link href="/">
+            <h1 className="cursor-pointer text-white font-robotoMonoBold hover:text-black py-2 border-b border-black">
+              Contact
             </h1>
           </Link>
         </div>
-      </div>
+      )}
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="relative mx-auto">
+    <div className="relative">
       <Head>
         <title>FilBasket - Filipino Basketball League</title>
         <link rel="icon" href="/favicon.ico" />
