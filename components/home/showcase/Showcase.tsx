448f00f8c073abf9469f3ff236af0a909ee0df99
Paul Buscano <pbuscano@deloitte.com>
2022-10-16 19:05:07 +0800

    Added showcase

--- components/home/showcase/Showcase.tsx
+++ components/home/showcase/Showcase.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import bg from "../../../styles/images/bg-showcase.jpeg";
+import bg from "../../../styles/images/bg-showcase-nba.png";
 
 interface Props {}
 
@@ -16,10 +16,7 @@ function Showcase({}: Props) {
         height: "100%",
       }}
     >
-      <img
-        style={{ width: 800 }}
-        src="https://scontent.fmnl4-1.fna.fbcdn.net/v/t39.30808-6/306959601_210123531354865_7108149813088365516_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEavIeD8_sxgKPCYNVO0E5z3-c2hHlmLeDf5zaEeWYt4K27mf1dbQaz9SDlEVYKBamgs16L1EM3r3tTJWCSbqcb&_nc_ohc=ovK0x-DJJzgAX9keMXw&_nc_ht=scontent.fmnl4-1.fna&oh=00_AT9Pw4q8Foe7Zd-9AP4oJI9is8pYETQ1NSudJttIiFDOwA&oe=635188A9"
-      />
+      <img style={{ width: "100%" }} src={bg.src} />
     </div>
   );
 }
