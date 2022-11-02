b4ed2c2723ccbd19a9b88a79886d0dd5a4f49408
Paul Buscano <pbuscano@deloitte.com>
2022-11-02 14:06:05 +0800

    Add related article in team details

--- pages/teams/[slug].tsx
+++ pages/teams/[slug].tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TeamHeader from "../../components/teamDetails/TeamHeader";
+import TeamRelatedNews from "../../components/teamDetails/TeamRelatedNews";
 import TeamTabs from "../../components/teamDetails/TeamTabs";
 
 type Props = {};
@@ -9,6 +10,7 @@ const Teams = (props: Props) => {
     <div className="max-w-screen-2xl mx-auto px-3">
       <TeamHeader />
       <TeamTabs />
+      <TeamRelatedNews />
     </div>
   );
 };
